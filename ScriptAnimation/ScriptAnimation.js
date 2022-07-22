61885165c7186dbd5c9975d6e38d43b4da39cfd8
KimandoHs <85370569+KimandoHs@users.noreply.github.com>
2022-07-22 02:26:16 -0400

    animation basics

--- ScriptAnimation/ScriptAnimation.js
+++ ScriptAnimation/ScriptAnimation.js
@@ -7,22 +7,22 @@ function stopAnimate() {
 
 function animateScript() {
 
-  var position = 96; //start position for the image slicer
-  const interval = 100; //100 ms of interval for the setInterval()
-  const diff = 96; //diff as a variable for position offset
+  var position = 64; //start position for the image slicer
+  const interval = 20; //100 ms of interval for the setInterval()
+  const diff = 64; //diff as a variable for position offset
 
   tID = setInterval(() => {
 
     document.getElementById("image").style.backgroundPosition =
-      `-${position}px 100px`;
+      `-${position}px 0px`;
     //we use the ES6 template literal to insert the variable "position"
 
-    if (position < 960) {
+    if (position < 1280) {
       position = position + diff;
     }
     //we increment the position by 256 each time
     else {
-      position = 96;
+      position = 64;
     }
     //reset the position to 256px, once position exceeds 1536px
 
