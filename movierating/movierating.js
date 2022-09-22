003a9636479e85dafe41742032ebaa48226fbd7e
KimandoHs <85370569+KimandoHs@users.noreply.github.com>
2022-09-22 09:43:02 -0400

    added another icon to the website or so they say

---
+++ movierating/movierating.js
@@ -0,0 +1,5 @@
+const myHeading = document.getElementById('firstheading');
+myHeading.textContent = 'Hello world!';
+
+const myHeading2 = document.getElementById('secondheading');
+myHeading2.textContent = 'Hello world! This is working.';
